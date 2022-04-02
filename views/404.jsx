890d9105d90c961cb52a6a67edc136932b678ab4
CKline22 <CKline.1022@gmail.com>
2022-04-02 09:26:36 -0400

    adding bootstrap grid to index page

--- views/404.jsx
+++ views/404.jsx
@@ -7,6 +7,13 @@ function error404 () {
             <main>
                 <h1>404: Page Not Found</h1>
                 <p>Sorry we can't find this page</p>
+                <div>
+                    <img src="/images/stormtrooper-walking-desert.jpg" alt="stormtrooper lego walking in the desert"/>
+                    <div>
+                    Photo by <a href="https://unsplash.com/@danielkcheung?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Daniel K Cheung</a> on <a href="https://unsplash.com/s/photos/funny?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                    </div>
+                </div>
             </main>
         </Def>
     )
